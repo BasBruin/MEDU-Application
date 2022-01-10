19cdfd28adf71690d5d35183ab02d37d30d692fc
Bas Bruin <487790@student.fontys.nl>
2022-01-10 14:01:18 +0100

    Ik heb van alle hamburgermenu's eentje gemaakt en wat comments neergezet

--- MEDU Application/Patient.cs
+++ MEDU Application/Patient.cs
@@ -26,6 +26,7 @@ namespace MEDU_Application
         public string Username { get; }
         public string Password { get; }
 
+        // Hier maak ik de constructor aan waar alle info in gaat van de user.
         public Patient(int Id, string Voornaam, string Achternaam, int Leeftijd, string Bsn, string Email, string Telefoonnummer, string Allergiën ,  int Dokter_id)
         {
             this.Id = Id;
@@ -39,27 +40,17 @@ namespace MEDU_Application
             this.Allergiën = Allergiën;
         }
 
+        // Deze constructor is voor de username en password door te geven 
         public Patient(string Username, string Password)
         {
             this.Username = Username;
             this.Password = Password;
         }
 
-        public Patient()
-        {
-            this.Id = Id;
-            this.Voornaam = Voornaam;
-            this.Achternaam = Achternaam;
-            this.Leeftijd = Leeftijd;
-            this.Email = Email;
-            this.Telefoonnummer = Telefoonnummer;
-            this.Dokter_id = Dokter_id;
-            this.Bsn = Bsn;
-            this.Allergiën = Allergiën;
-        }
-
+        // Deze Boolean kijkt of de username en password hetzelfde zijn als die in de database en op basis daarvan geeft hij een true of false terug
         public Boolean inloggen()
         {
+            //Dit stukje zorgt voor de connectie met de database
             string connString = "datasource=127.0.0.1;port=3306;username=root;password=;database=sql_database;";
             string query = string.Format("SELECT * FROM patient WHERE username='{0}' AND password='{1}'", this.Username, this.Password);
             MySqlConnection databaseConnection = new MySqlConnection(connString);
@@ -70,7 +61,7 @@ namespace MEDU_Application
             dt = new DataTable(); //this is creating a virtual table  
             dt.Load(cmd.ExecuteReader());
 
-
+            // Als de datatable terug komt met 1 row betekent dat er een profiel is met die specifieke username en password en mag je dus inloggen.
             if (dt.Rows.Count == 1)
             {
                 return true;
--- MEDU Application/Vandaag.cs
+++ MEDU Application/Vandaag.cs
@@ -66,7 +66,7 @@ namespace MEDU_Application
             cbIngenomen.Checked = false; // de code hier boven maakt de labels in de presciptie group box leeg en zet de combo box als standaard false
 
             string gekozenDate = lbxTijd.GetItemText(lbxTijd.SelectedItem); // zet de geselecteerde item uit de date listbox in een string genaamd date
-            Console.WriteLine(gekozenDate);
+
 
             reader = GetDataBase.loadSQL("SELECT cast(consumption_date.date as date) as dt_Date, cast(consumption_date.date as TIME) as dt_Time, medicine.name FROM `consumption_date`, medicine WHERE cast(consumption_date.date as date) = CURRENT_DATE AND consumption_date.medicine_id = medicine.id AND consumption_date.patient_id =  '" + 1 + "' AND cast(consumption_date.date as TIME) ='" + gekozenDate + "';"); // ropet de medcijn op die is gekoppeld aan de datum en id van de database
             if (reader.HasRows)
@@ -86,8 +86,7 @@ namespace MEDU_Application
         {
             string gekozenMedicijn = lbxMedicijn.GetItemText(lbxMedicijn.SelectedItem); // zet de geselecteerde item in de medicijn listbox in een string
             string gekozenDate = lbxTijd.GetItemText(lbxTijd.SelectedItem); // zet de geselecteerde item uit de date listbox in een string genaamd date
-            Console.WriteLine(gekozenMedicijn);
-            Console.WriteLine(gekozenDate);
+
 
             reader = GetDataBase.loadSQL("SELECT cast(consumption_date.date as date) as dt_Date, cast(consumption_date.date as TIME) as dt_Time, medicine.name, medicine.consumption_method, consumption_date.amount, consumption_date.is_consumed FROM consumption_date, medicine WHERE cast(consumption_date.date as date) = CURRENT_DATE AND consumption_date.medicine_id = medicine.id AND medicine.name = '" + gekozenMedicijn + "' AND consumption_date.patient_id = '" + 1 + "' AND cast(consumption_date.date as TIME) ='" + gekozenDate + "';"); // roept de medcijn, dose, inname methode en of het al is ingenomen op die is gekoppeld aan de datum, id en de medicijn naam van de database
             if (reader.HasRows)
@@ -113,8 +112,7 @@ namespace MEDU_Application
             string gekozenDate = lbxTijd.GetItemText(lbxTijd.SelectedItem);// zet de geselecteerde item in de medicijn listbox in een string
             string gekozenMedicijn = lbxMedicijn.GetItemText(lbxMedicijn.SelectedItem);// zet de geselecteerde item uit de date listbox in een string genaamd date
             bool ingenomen = cbIngenomen.Checked; //zet de value van de checkbox in een bool genaamd ingenomen
-            Console.WriteLine(gekozenMedicijn);
-            Console.WriteLine(gekozenDate);
+
 
             reader = GetDataBase.loadSQL("UPDATE consumption_date SET is_consumed = " + ingenomen + " WHERE cast(consumption_date.date as TIME) = '" + gekozenDate + "' AND consumption_date.patient_id = " + 1 + "; "); // deze query stuurt een update naar de database waar de patient id en de datum over een komen om aan de waarde van is_cosumed te veranderen naar de waarde van 'ingenomen'
             reader = GetDataBase.loadSQL("SELECT cast(consumption_date.date as date) as dt_Date, cast(consumption_date.date as TIME) as dt_Time, medicine.name, medicine.consumption_method, consumption_date.amount, consumption_date.is_consumed FROM consumption_date, medicine WHERE cast(consumption_date.date as date) = CURRENT_DATE AND consumption_date.medicine_id = medicine.id AND medicine.name = '" + gekozenMedicijn + "' AND consumption_date.patient_id = '" + 1 + "' AND cast(consumption_date.date as TIME) ='" + gekozenDate + "';"); // roept de medcijn, dose, inname methode en of het al is ingenomen op die is gekoppeld aan de datum, id en de medicijn naam van de database
